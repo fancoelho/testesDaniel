1cb41e116509f7b14df23b64bc726ba5b619a7dd
fancoelho <fancoelho@DESKTOP-UFRGFQM.home>
2022-01-31 12:28:15 +0000

    first version

---
+++ textService/src/main/java/com/tratamentoTexto/textService/application/TextServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tratamentoTexto.textService.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextServiceApplication.class, args);
+	}
+
+}
